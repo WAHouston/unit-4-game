7a60e4274b7330a1945c43bc7293c48c362608bd
Will H <wandrewhouston@gmail.com>
2018-09-18 14:21:24 -0400

    added color change and movement for character selections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,43 @@
-var jcHp = 120
-var hdHp = 150
-var hpHp = 100
-var dsHp = 180
-var jcPow = 
-var hdPow = 
-var hpPow = 
-var dsPow = 
-var jcCounter = 
-var hdCounter = 
-var hpCounter = 
-var dsCounter = 25
+var constantine = {
+    baseHp: 10,
+    basePow: 1,
+    hp: 10,
+    pow: 1,
+    counter: 1
+}
+
+var dresden = {
+    baseHp: 10,
+    basePow: 1,
+    hp: 10,
+    pow: 1,
+    counter: 1
+}
+
+var potter = {
+    baseHp: 10,
+    basePow: 1,
+    hp: 10,
+    pow: 1,
+    counter: 1
+}
+
+var strange = {
+    baseHp: 10,
+    basePow: 1,
+    hp: 10,
+    pow: 1,
+    counter: 1
+}
+
+var choices = $(".char").click(function() {
+    if ($(event.currentTarget).parent()[0] === $("#select")[0]) {
+        $(event.currentTarget).addClass("chosen").removeClass("neutral")
+        $("#yourchar").append(event.currentTarget)
+        $("#enemies").append($("#select").children())
+    } else if ($(event.currentTarget).parent()[0] === $("#enemies")[0] && $("#defender").children().length === 0) {
+        $(event.currentTarget).addClass("enemy").removeClass("neutral")
+        $("#defender").append(event.currentTarget)
+    }
+})
+
